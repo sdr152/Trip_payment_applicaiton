984447957d18edca90bd3a77ab743cd9ef648c68
Samuel Ramos <samueldavidrg152@gmail.com>
2023-02-16 11:03:01 -0600

    Add new csharp project

---
+++ Form1.cs
@@ -0,0 +1,44 @@
+namespace GuiDemo;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+        MessageBox.Show("Welcome to SimpliLearn");
+    }
+
+    private void button1_Click_1(object sender, EventArgs e)
+    {
+        MessageBox.Show("Button Click");
+    }
+
+    private void label2_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void label1_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void textBox1_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    private void textBox2_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    private void textBox3_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+}
