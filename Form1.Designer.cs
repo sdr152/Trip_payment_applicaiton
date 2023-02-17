ffa1a0ef953eec8f185658d1a1aa15bfe03afe3e
unknown <samueldavidrg152@gmail.com>
2023-02-16 19:24:48 -0600

    Modify project

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -51,13 +51,17 @@ partial class Form1
             this.textBox9 = new System.Windows.Forms.TextBox();
             this.textBox10 = new System.Windows.Forms.TextBox();
             this.textBox11 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label12 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 608);
+            this.button1.Location = new System.Drawing.Point(10, 456);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.Size = new System.Drawing.Size(82, 22);
             this.button1.TabIndex = 0;
             this.button1.Text = "Generar";
             this.button1.UseVisualStyleBackColor = true;
@@ -66,9 +70,9 @@ partial class Form1
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 57);
+            this.label1.Location = new System.Drawing.Point(10, 43);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(118, 20);
+            this.label1.Size = new System.Drawing.Size(94, 15);
             this.label1.TabIndex = 1;
             this.label1.Text = "Trayecto en Km: ";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -76,9 +80,9 @@ partial class Form1
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 92);
+            this.label2.Location = new System.Drawing.Point(10, 69);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(141, 20);
+            this.label2.Size = new System.Drawing.Size(111, 15);
             this.label2.TabIndex = 2;
             this.label2.Text = "Precio Diesel Galón:";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -86,42 +90,45 @@ partial class Form1
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 126);
+            this.label3.Location = new System.Drawing.Point(10, 94);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(145, 20);
+            this.label3.Size = new System.Drawing.Size(115, 15);
             this.label3.TabIndex = 3;
             this.label3.Text = "Precio Depreciación:";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(163, 50);
+            this.textBox1.Location = new System.Drawing.Point(143, 38);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(125, 27);
+            this.textBox1.Size = new System.Drawing.Size(110, 23);
             this.textBox1.TabIndex = 4;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(163, 85);
+            this.textBox2.Location = new System.Drawing.Point(143, 64);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(125, 27);
+            this.textBox2.Size = new System.Drawing.Size(110, 23);
             this.textBox2.TabIndex = 5;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(163, 119);
+            this.textBox3.Location = new System.Drawing.Point(143, 89);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(125, 27);
+            this.textBox3.Size = new System.Drawing.Size(110, 23);
             this.textBox3.TabIndex = 6;
             this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 188);
+            this.label4.Location = new System.Drawing.Point(10, 141);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(179, 20);
+            this.label4.Size = new System.Drawing.Size(142, 15);
             this.label4.TabIndex = 1;
             this.label4.Text = "Costo al 25% Retorno 0%:";
             this.label4.Click += new System.EventHandler(this.label1_Click);
@@ -129,9 +136,9 @@ partial class Form1
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 223);
+            this.label5.Location = new System.Drawing.Point(10, 167);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(179, 20);
+            this.label5.Size = new System.Drawing.Size(142, 15);
             this.label5.TabIndex = 2;
             this.label5.Text = "Costo al 50% Retorno 0%:";
             this.label5.Click += new System.EventHandler(this.label2_Click);
@@ -139,66 +146,71 @@ partial class Form1
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(12, 257);
+            this.label6.Location = new System.Drawing.Point(10, 193);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(231, 20);
+            this.label6.Size = new System.Drawing.Size(183, 15);
             this.label6.TabIndex = 3;
             this.label6.Text = "Costo a más del 50% Retorno 0%:";
             this.label6.Click += new System.EventHandler(this.label6_Click);
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(248, 181);
+            this.textBox4.Location = new System.Drawing.Point(217, 136);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(125, 27);
+            this.textBox4.Size = new System.Drawing.Size(110, 23);
             this.textBox4.TabIndex = 4;
             this.textBox4.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(248, 216);
+            this.textBox5.Location = new System.Drawing.Point(217, 162);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(125, 27);
+            this.textBox5.Size = new System.Drawing.Size(110, 23);
             this.textBox5.TabIndex = 5;
             this.textBox5.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(248, 250);
+            this.textBox6.Location = new System.Drawing.Point(217, 188);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(125, 27);
+            this.textBox6.Size = new System.Drawing.Size(110, 23);
             this.textBox6.TabIndex = 6;
             this.textBox6.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // textBox7
             // 
-            this.textBox7.Location = new System.Drawing.Point(248, 342);
+            this.textBox7.Location = new System.Drawing.Point(217, 256);
+            this.textBox7.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(125, 27);
+            this.textBox7.Size = new System.Drawing.Size(110, 23);
             this.textBox7.TabIndex = 10;
             // 
             // textBox8
             // 
-            this.textBox8.Location = new System.Drawing.Point(248, 308);
+            this.textBox8.Location = new System.Drawing.Point(217, 231);
+            this.textBox8.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(125, 27);
+            this.textBox8.Size = new System.Drawing.Size(110, 23);
             this.textBox8.TabIndex = 9;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(12, 349);
+            this.label7.Location = new System.Drawing.Point(10, 262);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(153, 20);
+            this.label7.Size = new System.Drawing.Size(121, 15);
             this.label7.TabIndex = 8;
             this.label7.Text = "Costo comida por dia";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(12, 315);
+            this.label8.Location = new System.Drawing.Point(10, 236);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(160, 20);
+            this.label8.Size = new System.Drawing.Size(125, 15);
             this.label8.TabIndex = 7;
             this.label8.Text = "Costo operario por dia";
             this.label8.Click += new System.EventHandler(this.label8_Click);
@@ -206,9 +218,9 @@ partial class Form1
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(13, 418);
+            this.label9.Location = new System.Drawing.Point(11, 314);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(99, 20);
+            this.label9.Size = new System.Drawing.Size(78, 15);
             this.label9.TabIndex = 1;
             this.label9.Text = "Cobrar 0 - 25:";
             this.label9.Click += new System.EventHandler(this.label1_Click);
@@ -216,9 +228,9 @@ partial class Form1
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(13, 453);
+            this.label10.Location = new System.Drawing.Point(11, 340);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(107, 20);
+            this.label10.Size = new System.Drawing.Size(84, 15);
             this.label10.TabIndex = 2;
             this.label10.Text = "Cobrar 25 - 50:";
             this.label10.Click += new System.EventHandler(this.label2_Click);
@@ -226,42 +238,81 @@ partial class Form1
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(13, 487);
+            this.label11.Location = new System.Drawing.Point(11, 365);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(115, 20);
+            this.label11.Size = new System.Drawing.Size(90, 15);
             this.label11.TabIndex = 3;
             this.label11.Text = "Cobrar 50 - 100:";
             this.label11.Click += new System.EventHandler(this.label6_Click);
             // 
             // textBox9
             // 
-            this.textBox9.Location = new System.Drawing.Point(248, 411);
+            this.textBox9.Location = new System.Drawing.Point(217, 308);
+            this.textBox9.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox9.Name = "textBox9";
-            this.textBox9.Size = new System.Drawing.Size(125, 27);
+            this.textBox9.Size = new System.Drawing.Size(110, 23);
             this.textBox9.TabIndex = 4;
             this.textBox9.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox10
             // 
-            this.textBox10.Location = new System.Drawing.Point(248, 446);
+            this.textBox10.Location = new System.Drawing.Point(217, 334);
+            this.textBox10.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox10.Name = "textBox10";
-            this.textBox10.Size = new System.Drawing.Size(125, 27);
+            this.textBox10.Size = new System.Drawing.Size(110, 23);
             this.textBox10.TabIndex = 5;
             this.textBox10.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // textBox11
             // 
-            this.textBox11.Location = new System.Drawing.Point(248, 480);
+            this.textBox11.Location = new System.Drawing.Point(217, 360);
+            this.textBox11.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox11.Name = "textBox11";
-            this.textBox11.Size = new System.Drawing.Size(125, 27);
+            this.textBox11.Size = new System.Drawing.Size(110, 23);
             this.textBox11.TabIndex = 6;
             this.textBox11.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(45, 12);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(73, 19);
+            this.radioButton1.TabIndex = 11;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Carretera";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(157, 12);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(63, 19);
+            this.radioButton2.TabIndex = 12;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Ciudad";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label12.Location = new System.Drawing.Point(395, -2);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(217, 37);
+            this.label12.TabIndex = 13;
+            this.label12.Text = "COBRO DE VIAJE";
+            this.label12.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1131, 649);
+            this.ClientSize = new System.Drawing.Size(990, 487);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.textBox7);
             this.Controls.Add(this.textBox8);
             this.Controls.Add(this.label7);
@@ -285,6 +336,7 @@ partial class Form1
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -317,4 +369,7 @@ partial class Form1
     private TextBox textBox9;
     private TextBox textBox10;
     private TextBox textBox11;
+    private RadioButton radioButton1;
+    private RadioButton radioButton2;
+    private Label label12;
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,6 @@
+using System.Reflection.Metadata;
+using System.Reflection.Metadata.Ecma335;
+
 namespace GuiDemo;
 
 public partial class Form1 : Form
@@ -27,19 +30,34 @@ public partial class Form1 : Form
 
     }
 
+    // Trayectoria km
     private void textBox1_TextChanged(object sender, EventArgs e)
     {
-
+        string txt = textBox1.Text;
     }
 
+    // Precio Diesel galon
     private void textBox2_TextChanged(object sender, EventArgs e)
     {
+        int i = 0;
+        if (double.TryParse(textBox2.Text, out double d) || int.TryParse(textBox2.Text, out i))
+        {
+            double precio_dep = Convert.ToDouble(textBox2.Text) * 0.25;
+            textBox3.Text = precio_dep.ToString();
+        }
+        else
+        {
+            textBox2.Clear();
+            textBox3.Clear();
+        }
+        
+        
 
     }
 
     private void textBox3_TextChanged(object sender, EventArgs e)
     {
-
+        
     }
 
     private void label6_Click(object sender, EventArgs e)
